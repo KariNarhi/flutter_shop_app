b017cced242a89460e97fb93d8b390846711b4c5
Kari Närhi <Kari.Narhi@tecinspire.com>
2021-05-16 23:38:18 +0300

    Working with shopping cart & display total

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 
 import './screens/products_overview_screen.dart';
 import './screens/product_detail_screen.dart';
+import './screens/cart_screen.dart';
 import './providers/cart.dart';
 import './providers/products.dart';
 
@@ -32,6 +33,7 @@ class MyApp extends StatelessWidget {
         home: ProductsOverviewScreen(),
         routes: {
           ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+          CartScreen.routeName: (ctx) => CartScreen(),
         },
       ),
     );
