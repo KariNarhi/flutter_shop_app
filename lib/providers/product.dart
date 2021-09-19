34fa3d5ea36fd5bb721333d358745d1f80946990
Kari Närhi <Kari.Narhi@tecinspire.com>
2021-09-19 19:07:10 +0300

    update favorite status optimistically

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class Product with ChangeNotifier {
+  String domain =
+      "flutter-dart-course-200d4-default-rtdb.europe-west1.firebasedatabase.app";
+
   final String id;
   final String title;
   final String description;
@@ -17,8 +22,28 @@ class Product with ChangeNotifier {
     this.isFavorite = false,
   });
 
-  void toggleFavoriteStatus() {
+  void _setFavValue(bool newValue) {
+    isFavorite = newValue;
+    notifyListeners();
+  }
+
+  Future<void> toggleFavoriteStatus() async {
+    final oldStatus = isFavorite;
     isFavorite = !isFavorite;
     notifyListeners();
+    final url = Uri.https(domain, "/products/$id.json");
+    try {
+      final res = await http.patch(
+        url,
+        body: json.encode({
+          "isFavorite": isFavorite,
+        }),
+      );
+      if (res.statusCode >= 400) {
+        _setFavValue(oldStatus);
+      }
+    } catch (error) {
+      _setFavValue(oldStatus);
+    }
   }
 }
