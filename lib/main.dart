c14bc4c04eb7da6032c379ad25a2ba1b5bff5916
Kari Närhi <Kari.Narhi@tecinspire.com>
2021-10-17 19:35:26 +0300

    Adding User Signup

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import './screens/edit_product_screen.dart';
 import './screens/cart_screen.dart';
 import './screens/orders_screen.dart';
 import './screens/auth_screen.dart';
+import './providers/auth.dart';
 import './providers/cart.dart';
 import './providers/products.dart';
 import './providers/orders.dart';
@@ -21,6 +22,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
+        ChangeNotifierProvider(
+          create: (ctx) => Auth(),
+        ),
         ChangeNotifierProvider(
           create: (ctx) => Products(),
         ),
